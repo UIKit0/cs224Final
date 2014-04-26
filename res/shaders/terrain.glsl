03e615b405b38b657541521a59f79caf0233380c
jixwanwang <jixuan_wang@brown.edu>
2014-04-26 17:37:34 -0400

    forgot terrain shader

---
+++ res/shaders/terrain.glsl
@@ -0,0 +1,22 @@
+Smoke Shading
+
+-- vertex ---------------------------------------
+
+uniform mat4 proj_matrix;
+uniform mat4 mv_matrix;
+
+in vec3 position;
+
+void main(void)
+{
+    gl_Position = proj_matrix * mv_matrix * vec4(position, 1.0);
+}
+
+-- fragment ---------------------------------------
+
+out vec4 color;
+
+void main(void)
+{
+    color = vec4(0.5, 0.5, 0.5, 1.0);
+}

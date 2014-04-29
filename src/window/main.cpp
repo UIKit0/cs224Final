e628a2251ee1fcb5858db4d0075df45abb230880
ArthurYidi <ayidi@risd.edu>
2014-04-29 14:19:59 -0400

    older textures back and misc fixes

--- src/window/main.cpp
+++ src/window/main.cpp
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 #endif
 
     window.show();
+//    window.showFullScreen();
     // will tick according to sreen refresh screen rate
     window.setAnimating(true);
 

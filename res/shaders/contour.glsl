3239ecbe2ba7ab2414b349aac6fb829109f30cc0
ArthurYidi <ayidi@risd.edu>
2014-05-02 07:24:02 -0400

    bleghh contour

--- res/shaders/contour.glsl
+++ res/shaders/contour.glsl
@@ -105,10 +105,10 @@ void main(void)
 // shader from pez (works much better than method from gooch)
 
 layout(triangles_adjacency) in;
-layout(points, max_vertices = 12) out;
+layout(triangle_strip, max_vertices = 12) out;
 // 3 edges x 4 quads = 12 vertices
 
-uniform float HalfWidth = 0.005;
+uniform float HalfWidth = 0.002;
 uniform float OverhangLength = 0.15;
 
 bool IsFront(vec3 A, vec3 B, vec3 C)
@@ -152,17 +152,17 @@ void main()
     }
 }
 
--- geometry2 ---------------------------------------------------------
+-- geometry2.points ---------------------------------------------------------
 
 layout(triangles_adjacency) in;
-layout(triangle_strip, max_vertices = 6) out;
+layout(points, max_vertices = 6) out;
 // 3 edges x 4 quads = 12 vertices
 
 uniform mat4 vp_matrix;
 uniform vec3 viewVec;
 
 uniform float offsetDepth = 0.15;
-uniform float hstrokeWidth = 0.01;
+uniform float hstrokeWidth = 0.008;
 
 in V_OUT
 {
@@ -235,13 +235,117 @@ void checkContour(vec3 nface1, int vId1, int vId2, int vId3)
     // silohuette
 
     // equation from gooch
-    float contour = ( dot(nface1, nface2));
+    float contour = ( dot(nface2, viewVec));
 
-    if (contour > 0) {
-        if (contour < 0.8)
-            emitEdge(vId1,vId2,vId3, true);
-        else
-            emitEdge(vId1,vId2,vId3, false);
+    if (contour >= 0) {
+        emitEdge(vId1,vId2,vId3, false);
+    }
+
+    // TODO: creases ?
+}
+
+void main(void)
+{
+    // get face normal (0,2,4) [ triangle ]
+    // adjecent indices (5,1,3)
+
+    vec3 v0 = gl_in[0].gl_Position.xyz;
+    vec3 v2 = gl_in[2].gl_Position.xyz;
+    vec3 v4 = gl_in[4].gl_Position.xyz;
+
+    vec3 nface = normalize(cross(v2 - v0, v4 - v0));
+
+    float viewDotN = dot(nface, viewVec);
+
+    // check if triangle is front-facing
+    if (viewDotN > 0) {
+        checkContour(nface, 2,0,1);
+        checkContour(nface, 4,2,3);
+        checkContour(nface, 0,4,5);
+    }
+}
+
+-- geometry2.tri ---------------------------------------------------------
+
+layout(triangles_adjacency) in;
+layout(triangle_strip, max_vertices = 6) out;
+// 3 edges x 4 quads = 12 vertices
+
+uniform mat4 vp_matrix;
+uniform vec3 viewVec;
+
+uniform float offsetDepth = 0.15;
+uniform float hstrokeWidth = 0.01;
+
+in V_OUT
+{
+    vec4 normal;
+} v_in[];
+
+out G_OUT
+{
+    vec3 color;
+} g_out;
+
+void emitEdge(int vId1, int vId2, int vId3, bool isCrease)
+{
+    vec3 clr = vec3(0,0,0);
+    if (isCrease)
+        clr = vec3(1,0,0);
+
+    vec3 wsv1 = gl_in[vId1].gl_Position.xyz;
+    vec3 wsv2 = gl_in[vId2].gl_Position.xyz;
+    vec3 wsv3 = gl_in[vId3].gl_Position.xyz;
+
+    vec3 nv1 = v_in[vId1].normal.xyz;
+    vec3 nv2 = v_in[vId2].normal.xyx;
+
+    // bottom left
+    vec3 v2 = wsv2 + nv2 * hstrokeWidth;
+    g_out.color = clr;
+    gl_Position = vp_matrix * vec4(v2, 1);
+    gl_Position.z += offsetDepth;
+    EmitVertex();
+
+    // top left
+     v2 = wsv2;
+    g_out.color = clr;
+    gl_Position = vp_matrix * vec4(v2, 1);
+    gl_Position.z += offsetDepth;
+    EmitVertex();
+
+    // bottom right
+    vec3 v1 = wsv1 + nv1 * hstrokeWidth;
+    g_out.color = clr;
+    gl_Position = vp_matrix * vec4(v1, 1);
+    gl_Position.z += offsetDepth;
+    EmitVertex();
+
+    // top right
+     v1 = wsv1;
+    g_out.color = clr;
+    gl_Position = vp_matrix * vec4(v1, 1);
+    gl_Position.z += offsetDepth;
+    EmitVertex();
+
+    EndPrimitive();
+}
+
+void checkContour(vec3 nface1, int vId1, int vId2, int vId3)
+{
+    vec3 v1 = gl_in[vId1].gl_Position.xyz;
+    vec3 v2 = gl_in[vId2].gl_Position.xyz;
+    vec3 v3 = gl_in[vId3].gl_Position.xyz;
+
+    vec3 nface2 = normalize(cross(v2 - v3, v1 - v3));
+
+    // silohuette
+
+    // equation from gooch
+    float contour = ( dot(nface2, viewVec));
+
+    if (contour >= 0) {
+        emitEdge(vId1,vId2,vId3, false);
     }
 
     // TODO: creases ?
--- src/scene/contour.cpp
+++ src/scene/contour.cpp
@@ -1,5 +1,8 @@
 #include "contour.h"
 
+#define GOOD_CONTOUR
+//#define BAD_CONTOUR
+
 Contour::Contour()
     : m_gl(NULL)
 {
@@ -20,13 +23,24 @@ void Contour::initialize(GLFunctions *gl, Obj &mesh)
     m_goochFx.link();
 
     m_contourFx.initialize(gl);
-//    m_contourFx.compile(GL_GEOMETRY_SHADER, "contour.geometry");
-//    m_contourFx.compile(GL_VERTEX_SHADER, "contour.vertex.mvp");
-//    m_contourFx.compile(GL_FRAGMENT_SHADER, "contour.fragment.black");
-    m_contourFx.compile(GL_GEOMETRY_SHADER, "contour.geometry2");
+#ifdef GOOD_CONTOUR
+    m_contourFx.compile(GL_GEOMETRY_SHADER, "contour.geometry");
+    m_contourFx.compile(GL_VERTEX_SHADER, "contour.vertex.mvp");
+    m_contourFx.compile(GL_FRAGMENT_SHADER, "contour.fragment.black");
+    m_contourFx.link();
+#endif
+#ifdef BAD_CONTOUR
+    m_contourFx.compile(GL_GEOMETRY_SHADER, "contour.geometry2.tri");
     m_contourFx.compile(GL_VERTEX_SHADER, "contour.vertex.line");
     m_contourFx.compile(GL_FRAGMENT_SHADER, "contour.fragment.color");
     m_contourFx.link();
+
+    m_contour2Fx.initialize(gl);
+    m_contour2Fx.compile(GL_GEOMETRY_SHADER, "contour.geometry2.points");
+    m_contour2Fx.compile(GL_VERTEX_SHADER, "contour.vertex.line");
+    m_contour2Fx.compile(GL_FRAGMENT_SHADER, "contour.fragment.color");
+    m_contour2Fx.link();
+#endif
 
     m_meshSize = mesh.triangles.size() * 3;
 
@@ -128,14 +142,20 @@ void Contour::initialize(GLFunctions *gl, Obj &mesh)
 
     gl->glVertexAttribPointer(m_contourFx.attrib("position"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
     gl->glEnableVertexAttribArray(m_contourFx.attrib("position"));
-
+#ifdef BAD_CONTOUR
+    gl->glVertexAttribPointer(m_contour2Fx.attrib("position"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
+    gl->glEnableVertexAttribArray(m_contour2Fx.attrib("position"));
+#endif
     offset += sizeof(glm::vec3);
 
     gl->glVertexAttribPointer(m_goochFx.attrib("normal"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
     gl->glEnableVertexAttribArray(m_goochFx.attrib("normal"));
-//    gl->glVertexAttribPointer(m_contourFx.attrib("normal"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
-//    gl->glEnableVertexAttribArray(m_contourFx.attrib("normal"));
-
+#ifdef BAD_CONTOUR
+    gl->glVertexAttribPointer(m_contourFx.attrib("normal"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
+    gl->glEnableVertexAttribArray(m_contourFx.attrib("normal"));
+    gl->glVertexAttribPointer(m_contour2Fx.attrib("normal"), 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (const void *) offset);
+    gl->glEnableVertexAttribArray(m_contour2Fx.attrib("normal"));
+#endif
     offset += sizeof(glm::vec3);
 
     //    gl->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(MeshBuffer), (void *)offsetof(MeshBuffer, texcoord));
@@ -160,8 +180,10 @@ void Contour::initialize(GLFunctions *gl, Obj &mesh)
 
 void Contour::draw()
 {
+#ifdef BAD_CONTOUR
     m_gl->glEnable(GL_PROGRAM_POINT_SIZE);
-    m_gl->glPointSize(2.0f);
+    m_gl->glPointSize(3.0f);
+#endif
 
     m_gl->glBindVertexArray(m_vao);
     m_gl->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_bufferIndex);
@@ -179,12 +201,26 @@ void Contour::draw()
 
     m_gl->glUseProgram(m_contourFx.program());
 
-//    m_gl->glUniformMatrix4fv(m_contourFx.uniform("mvp_matrix"), 1, GL_FALSE, glm::value_ptr(g_camera.pMatrix * g_camera.vMatrix * g_model.mMatrix));
+#ifdef GOOD_CONTOUR
+    m_gl->glUniformMatrix4fv(m_contourFx.uniform("mvp_matrix"), 1, GL_FALSE, glm::value_ptr(g_camera.pMatrix * g_camera.vMatrix * g_model.mMatrix));
+#endif
+
+#ifdef BAD_CONTOUR
     m_gl->glUniformMatrix4fv(m_contourFx.uniform("m_matrix"), 1, GL_FALSE, glm::value_ptr(g_model.mMatrix));
     m_gl->glUniformMatrix4fv(m_contourFx.uniform("vp_matrix"), 1, GL_FALSE, glm::value_ptr(g_camera.pMatrix * g_camera.vMatrix));
     m_gl->glUniform3fv(m_contourFx.uniform("viewVec"), 1, glm::value_ptr(g_camera.m_lookAt));
+#endif
 
     m_gl->glDrawElements(GL_TRIANGLES_ADJACENCY, m_meshSize * 2, GL_UNSIGNED_SHORT, 0);
 
+#ifdef BAD_CONTOUR
+    m_gl->glUseProgram(m_contour2Fx.program());
+
+    m_gl->glUniformMatrix4fv(m_contour2Fx.uniform("m_matrix"), 1, GL_FALSE, glm::value_ptr(g_model.mMatrix));
+    m_gl->glUniformMatrix4fv(m_contour2Fx.uniform("vp_matrix"), 1, GL_FALSE, glm::value_ptr(g_camera.pMatrix * g_camera.vMatrix));
+    m_gl->glUniform3fv(m_contour2Fx.uniform("viewVec"), 1, glm::value_ptr(g_camera.m_lookAt));
+
+    m_gl->glDrawElements(GL_TRIANGLES_ADJACENCY, m_meshSize * 2, GL_UNSIGNED_SHORT, 0);
+#endif
 //    m_gl->glDisable(GL_BLEND);
 }
